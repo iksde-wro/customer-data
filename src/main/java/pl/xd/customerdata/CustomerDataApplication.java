7dd52343f96c504f5710090415f964a0164c1e12
Tomek <tomaszkucharski95@gmail.com>
2023-08-18 13:07:35 +0200

    initial commit

---
+++ src/main/java/pl/xd/customerdata/CustomerDataApplication.java
@@ -0,0 +1,13 @@
+package pl.xd.customerdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerDataApplication.class, args);
+	}
+
+}
